13a40fef378e96decded3f6147b55c0180900c54
Nigar Okur <nigarr@gmail.com>
2018-06-12 11:57:19 -0400

    modal box added to show final score

--- js/app.js
+++ js/app.js
@@ -41,7 +41,8 @@ function shuffle(array) {
 
 let openCards = [];
 let moves = document.querySelector('.moves');
-let numOfMoves = parseInt(moves.innerText);
+let numOfMoves = parseInt(moves.innerText);    // number of moves made by user
+let counter = 0;   // to track the number of matched items
 
 deck.addEventListener('click', displayCard);
 
@@ -55,6 +56,12 @@ function displayCard(event) {
       openCards[0].classList.add('match');
       openCards[1].classList.add('match');
       openCards = [];
+      counter++;
+
+      // show the congratulations message to the user
+      if(counter === 1) {
+          displayFinalScore(getNumOfMoves());
+      }
     }
     else {
       setTimeout(function(){
@@ -72,4 +79,39 @@ function displayCard(event) {
 function updateMoves() {
   numOfMoves++;
   moves.innerText = numOfMoves;
+  return numOfMoves;
+}
+
+function getNumOfMoves() {
+  return updateMoves();
+}
+
+function displayFinalScore() {
+    modal.style.display = "block";
+    document.getElementById('list-moves').innerText = numOfMoves;
+    document.getElementById('list-time').innerText = "simdilik TIME yok";
+    document.getElementById('list-star').innerText = "simdilik STAR yok";
+}
+
+
+/*** MODAL ***/
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
